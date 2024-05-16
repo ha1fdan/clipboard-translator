6206b4388a4a3d27968ac0b366ea6306ca9c14a1
Ha1fdan <GamerHalfdan@gmail.com>
2024-05-16 09:17:18 +0200

    added src lang (input)

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ file = open(location_of_script + "/settings.conf", "r")
 for line in file:
     if line.startswith("DESTINATION_LANG"):
         dest_lang = line.split("=")[1].strip()
+    if line.startswith("SRC_LANG"):
+        src_lang = line.split("=")[1].strip()
 
 translator = Translator()
 text_to_translate = pyperclip.paste()
-translated_text = translator.translate(text_to_translate, dest=dest_lang).text
+translated_text = translator.translate(text_to_translate, dest=dest_lang, src=src_lang).text
 pyperclip.copy(translated_text)
